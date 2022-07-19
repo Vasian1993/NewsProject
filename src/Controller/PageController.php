bdeb81573b2a0a5251e425075289493eddcafc64
kuzmenko.v <qeqtySYB>
2022-07-19 21:59:44 +0700

    parser

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,22 +5,42 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class PageController extends AbstractController
 {
-
-   /**
-   * @Route("/page/number")
-    */
-    public function number(): Response
+    /**
+     * @Route("/parse/news")
+     */
+    public function parse(): Response
     {
-        $number = random_int(0, 100);
+        $url = "https://nsk.rbc.ru/";
 
-        return $this->render('page/number.html.twig', [
-            'number' => $number,
+        $client = HttpClient::create();
+
+        $response = $client->request('GET', $url);
+
+        $statusCode = $response->getStatusCode();
+
+        if($statusCode == '200'){
+            $content = $response->getContent();
+
+            $crawler = new Crawler($content);
+
+            $links = $crawler->filter('a.main__feed__link')->each(function ($node){
+                $href = $node->attr('href');
+                $title = $node->filter('span.main__feed__title')->text();
+
+                return compact('href', 'title');
+            });
+        }
+
+        return $this->render('page/news.html.twig', [
+            'links' => $links,
         ]);
     }
 }
